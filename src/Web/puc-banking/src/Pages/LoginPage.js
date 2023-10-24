352bc349fd3fa26e2b891426c739a62bf1062b43
RaulShinaede <rshinaede@outlook.com>
2023-10-24 14:59:14 -0300

    Subindo arquivos do app web

---
+++ src/Web/puc-banking/src/Pages/LoginPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../Components/Container';
+
+export default function LoginPage() {
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <Container>
+                <h1>Login Page</h1>
+            </Container>
+        </div>
+    );
+}
