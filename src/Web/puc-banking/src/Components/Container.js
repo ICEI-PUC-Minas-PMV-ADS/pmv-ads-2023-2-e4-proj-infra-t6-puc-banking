8796c536b3a7b7ebcb2a87f060db0881b038a6ad
Bruno Selas <bruno_sellas@hotmail.com>
2023-10-25 18:48:14 -0300

    criando estrutura JSx da página de login

--- src/Web/puc-banking/src/Components/Container.js
+++ src/Web/puc-banking/src/Components/Container.js
@@ -1,13 +1,39 @@
 import React from 'react';
+import pucImg from '/Users/bruno.selas.oikos/Documents/projetos/pmv-ads-2023-2-e4-proj-infra-t6-puc-banking/src/Web/puc-banking/src/assets/PucImg.png';
 
 export default function Container(props) {
     return (
-        <div style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 10,
-            border: '1px solid red'
-        }}>{props.children}</div>
+        <div className='container'>
+            <div className='container-login '>
+                <div className='wrap-login'>
+                    <form className='login-form'>
+                        <span className='login-form-title'>Bem Vindo!</span>
+                        <span className='login-form-login'>
+                            <img src={pucImg} alt='Logo Puc' />
+                        </span>
+
+                        <div className='wrap-input'>
+                            <input type='email' />
+                            <span className='focus-input' data-placeholder='Email'></span>
+                        </div>
+
+                        <div className='wrap-input'>
+                            <input type='password' />
+                            <span className='focus-input' data-placeholder='Password'></span>
+                        </div>
+
+                        <div className='container-login-form-btn'>
+                            <button className='login-form-btn'>Login</button>
+                        </div>
+
+                        <div className='text-center'>
+                            <span className='text1'>Não possui conta?</span>
+                            <a className='text2' href='#'>Criar Conta</a>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
