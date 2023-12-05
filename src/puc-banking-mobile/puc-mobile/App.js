65148f7dd9bf7c3335f965fe100d75839611bee9
RaulShinaede <rshinaede@outlook.com>
2023-12-05 13:57:47 -0300

    Subindo configuração de navegação do react native

--- src/puc-banking-mobile/puc-mobile/App.js
+++ src/puc-banking-mobile/puc-mobile/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider } from "native-base";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomePage from "./src/Pages/HomePage";
+import LoginPage from "./src/Pages/LoginPage";
+import CadastrePage from "./src/Pages/CadastrePage";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isSignedIn, setIsSignedIn] = React.useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {isSignedIn ? (
+              <>
+                <Stack.Screen name="Home" component={HomePage} />
+              </>
+            ) : (
+              <>
+                <Stack.Screen name="Login" component={LoginPage} />
+                <Stack.Screen name="Cadastre" component={CadastrePage} />
+              </>
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/puc-banking-mobile/puc-mobile/src/Pages/CadastrePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function CadastrePage() {
+  return (
+    <View>
+      <Text>Cadastre</Text>
+    </View>
+  );
+}
